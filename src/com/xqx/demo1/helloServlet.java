81e77f73c79d295620130892308fa286c12949a8
xingquanxiang <2515658674@qq.com>
2019-05-13 16:57:27 +0800

    第一次提交

---
+++ src/com/xqx/demo1/helloServlet.java
@@ -0,0 +1,27 @@
+package com.xqx.demo1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class helloServlet extends HttpServlet{
+	@Override
+	public void init() throws ServletException {
+		
+	}
+	
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	public void destroy() {
+		
+	}
+}
